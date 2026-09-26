b6eb4c0f8467207bd01b0ded4182d2c3c691fcaa
1Timess <167821617+1Timess@users.noreply.github.com>
2026-09-26 13:21:17 -0400

    Expose NEU pet constants path

--- src/server/reference/neu/paths.ts
+++ src/server/reference/neu/paths.ts
@@ -5,4 +5,6 @@ export function neuDataDirectory() {
 }
 export function neuRepositoryDirectory() { return path.join(neuDataDirectory(), "repository"); }
 export function neuItemsDirectory() { return path.join(neuRepositoryDirectory(), "items"); }
+export function neuConstantsDirectory() { return path.join(neuRepositoryDirectory(), "constants"); }
+export function neuPetConstantsPath() { return path.join(neuConstantsDirectory(), "pets.json"); }
 export function neuMetadataPath() { return path.join(neuDataDirectory(), "metadata.json"); }
